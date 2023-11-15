9363d7c7da4b54662825c6b4ba21f47921e321f7
0ofKim <pos00042@naver.com>
2023-11-15 23:10:00 +0900

    First Commit

---
+++ TodayLook/TodayLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayLookApp.swift
+//  TodayLook
+//
+//  Created by 0ofKim on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct TodayLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
