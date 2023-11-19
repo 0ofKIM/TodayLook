2b90d9837a55f83da7f8ca6491168c715bddc734
0ofKim <pos00042@naver.com>
2023-11-19 22:17:06 +0900

    Scene Delegate와 같은 역할 하는 코드 삽입

--- TodayLook/TodayLookApp.swift
+++ TodayLook/TodayLookApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct TodayLookApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("켜짐")
+            case .inactive:
+                print("꺼짐")
+            case .background:
+                print("백그라운드에서 돌아가는 중")
+            @unknown default:
+                fatalError()
+            }
         }
     }
 }
