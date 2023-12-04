87b8998aa6ea4e206ae22766a7906c5880493cb8
0ofKim <pos00042@naver.com>
2023-12-04 16:27:17 +0900

    메인 컨텐츠 TCA 적용

--- TodayLook/MainContentView.swift
+++ TodayLook/MainContentView.swift
@@ -9,88 +9,109 @@ import SwiftUI
 import ComposableArchitecture
 
 struct MainContentView: View {
+    let store: StoreOf<MainContentFeature>
     let columns = [GridItem(.flexible())]
-    
+
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns, alignment: .center, spacing: 20) {
-                    MainView()
-                    
-                    ForEach(1...20, id: \.self) { value in
-                        WeeklyWeatherView()
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            NavigationView {
+                ScrollView(.vertical) {
+                    LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
+                        testButton
+                        MainView()
+                        ForEach(1...5, id: \.self) { index in
+                            WeeklyWeatherView()
+                        }
                     }
                 }
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    settingToolbarItem
+                    titleToolbarItem
+                    bookmarkToolbarItem
+                }
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarBackground(Color(uiColor: .systemBlue), for: .navigationBar)
+                .toolbarColorScheme(.light, for: .navigationBar)
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                settingToolbarItem
-                titleToolbarItem
-                bookmarkToolbarItem
-            }
-            .toolbarBackground(.visible, for: .navigationBar)
-            .toolbarBackground(Color(uiColor: .systemBlue), for: .navigationBar)
-            .toolbarColorScheme(.light, for: .navigationBar)
         }
     }
 
-    var settingToolbarItem: ToolbarItem<(), Button<NavigationLink<Text, SettingView>>> {
+    var testButton: some View {
+        Button("(Test) Weather Change") {
+            store.send(.testTapped)
+        }
+        .frame(width: UIScreen.main.bounds.width, height: 30, alignment: .center)
+        .background(.red)
+    }
+
+    var settingToolbarItem: ToolbarItem<(), some View> {
         ToolbarItem(placement: .navigationBarLeading) {
-            Button(
-                action: {},
-                label: {
-                    NavigationLink(destination: SettingView()) {
-                        Text("설정")
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                }
-            )
-        }
-    }
-
-    var titleToolbarItem: ToolbarItem<(), HStack<Text>> {
-        ToolbarItem(placement: .principal) {
-            HStack {
-                Text("Navigation Title")
+            NavigationLink(destination: BookmarkView()) {
+                Text("즐겨찾기")
                     .bold()
                     .foregroundColor(.white)
             }
         }
     }
 
-    var bookmarkToolbarItem: ToolbarItem<(), Button<NavigationLink<Text, BookmarkView>>> {
+    var titleToolbarItem: ToolbarItem<(), some View> {
+        ToolbarItem(placement: .principal) {
+            Text("Navigation Title")
+                .bold()
+                .foregroundColor(.white)
+        }
+    }
+
+    var bookmarkToolbarItem: ToolbarItem<(), some View> {
         ToolbarItem(placement: .navigationBarTrailing) {
-            Button(
-                action: {},
-                label: {
-                    NavigationLink(destination: BookmarkView()) {
-                        Text("즐겨찾기")
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                }
-            )
+            NavigationLink(destination: BookmarkView()) {
+                Text("즐겨찾기")
+                    .bold()
+                    .foregroundColor(.white)
+            }
         }
     }
 }
 
-//struct MainContentFeature: Reducer {
-//    struct State {
-//        
-//    }
-//    
-//    enum Action {
-//        
-//    }
-//    
-//    var body: Body {
-//        
-//    }
-//}
+enum Weather {
+    case sunny
+    case rain
+}
+
+struct MainContentFeature: Reducer {
+    struct State: Equatable {
+        var weather: Weather
+    }
+    
+    enum Action {
+        case testTapped
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .testTapped:
+                if state.weather == .rain {
+                    state.weather = .sunny
+                } else {
+                    state.weather = .rain
+                }
+                print("current weather = \(state.weather)")
+                return .none
+            }
+        }
+    }
+}
 
 struct MainContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainContentView()
+        let state: MainContentFeature.State = MainContentFeature.State(weather: .sunny)
+
+        MainContentView(
+            store: Store(initialState: state) {
+                MainContentFeature()
+            }
+        )
     }
 }
