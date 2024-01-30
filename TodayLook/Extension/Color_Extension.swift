d4ff03280dfa4b7261bfedd7aa13c05945f710fb
0ofKim <pos00042@naver.com>
2024-01-30 23:36:18 +0900

    사이드시트 1차 개발

--- TodayLook/Extension/Color_Extension.swift
+++ TodayLook/Extension/Color_Extension.swift
@@ -9,6 +9,9 @@ import Foundation
 import SwiftUI
 
 extension Color {
+    static let Gray00: Color = Color(red: 1, green: 1, blue: 1)
     static let Gray01: Color = Color(red: 0.97, green: 0.97, blue: 0.97)
+    static let Gray04: Color = Color(red: 0.68, green: 0.68, blue: 0.68)
     static let Gray08: Color = Color(red: 0.06, green: 0.06, blue: 0.06)
+    
 }
--- TodayLook/UI/SettingContents/SettingView.swift
+++ TodayLook/UI/SettingContents/SettingView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("Setting View")
+        VStack(alignment: .leading, spacing: 4) {
+            HStack(alignment: .center, spacing: 0) {
+                Text("위치 설정")
+                    .font(Font.custom("Pretendard", size: 16))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.Gray08)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("Hi")
+                }, label: {
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("서울특별시 광진구")
+                            .font(Font.custom("Pretendard", size: 16))
+                            .lineLimit(1)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color.Gray04)
+                        Image("ChevronRight")
+                    }
+                })
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 12)
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            
+            HStack(alignment: .top, spacing: 0) {
+                Text("앱 정보")
+                    .font(Font.custom("Pretendard", size: 16))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.Gray08)
+                
+                Spacer()
+                
+                Text("v 1.0")
+                    .font(Font.custom("Pretendard", size: 16))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.Gray04)
+            }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+        }
+        .padding(.horizontal, 0)
+        .padding(.top, 84)
+        .padding(.bottom, 0)
+        .frame(width: 296, height: 844, alignment: .topLeading)
+        .background(Color.Gray01)
     }
 }
 
