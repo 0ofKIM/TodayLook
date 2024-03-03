5e2cf21b87dfc92cdc7b8e2a46be7fe3dc4e919f
0ofKim <pos00042@naver.com>
2024-03-03 23:14:08 +0900

    Reverse GeoCoding API & View Binding

--- TodayLook/Util/APIClient.swift
+++ TodayLook/Util/APIClient.swift
@@ -35,4 +35,36 @@ class APIClient {
         }
         task.resume()
     }
+    
+    func fetchReverseGeocoding(x: Double, y: Double, completion: @escaping (String) -> ()) {
+        guard !accessToken.isEmpty else {
+            print("Invalid AccessToken")
+            return
+        }
+        
+        let url: String = "https://sgisapi.kostat.go.kr/OpenAPI3/addr/rgeocodewgs84.json?accessToken=\(accessToken)&x_coor=\(x)&y_coor=\(y)&addr_type=20"
+        guard let url = URL(string: url) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error{
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            do{
+                let apiResponse = try JSONDecoder().decode(ReverseGeoCodeModel.self, from: data)
+                let area = "\(apiResponse.result?.first?.sido_nm ?? "") \(apiResponse.result?.first?.sgg_nm ?? "")"
+                completion(area)
+                print(apiResponse)
+            } catch(let err){
+                print(err.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 }
